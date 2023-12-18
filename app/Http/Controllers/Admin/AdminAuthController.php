e9d7c831ad9e599f5641f95c9ce7ffb365e74746
leonardo <bemmenos@gmail.com>
2023-12-18 16:36:34 -0300

    feat: adjuste forgot-password for admin area

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Password;
 
 class AdminAuthController extends Controller
 {
@@ -12,5 +13,30 @@ class AdminAuthController extends Controller
     {
         return view('Admin.auth.login');
     }
+
+
+    public function forgetPassword() : View
+    {
+        return view('Admin.auth.forgot-password');
+    }
+
+    public function forgetRequest(Request $request)
+    {
+
+        $request->validate([
+            'email' => ['required', 'email'],
+        ]);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $status == Password::RESET_LINK_SENT
+                    ? back()->with('status', __($status))
+                    : back()->withInput($request->only('email'))
+                            ->withErrors(['email' => __($status)]);
+
+    }
+
 
 }
--- routes/admin.php
+++ routes/admin.php
@@ -5,10 +5,11 @@ use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 
-
-
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
-    Route::get('/login', [AdminAuthController::class,'login'])->name('admin.login');
+    Route::get('/login', [AdminAuthController::class,'login'])->name('login');
+    Route::get('/forget-password', [AdminAuthController::class,'forgetPassword'])->name('login.forget-password');
+    Route::post('/forget-password', [AdminAuthController::class,'forgetRequest'])->name('password.request');
+
 });
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['user.type:admin']], function(){
